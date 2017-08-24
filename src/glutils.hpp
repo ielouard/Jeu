58489bfab974f0dc0f9baf7934910a1c908fe61a
Ibrahim elouard <ibrahim.elouard@cpe.fr>
2017-08-24 16:21:27 +0900

    translated

--- src/glutils.hpp
+++ src/glutils.hpp
@@ -1,12 +1,13 @@
 /*****************************************************************************\
- * TP CPE                                                                    *
+ * CPE Practical work                                                                    *
  * ------
  *
- * Fonctions d'aides pour OpenGL (affichage d'erreur, chargement de shaders, etc)
- * Code repris de David Odin
+ * Opengl help fonctions (Dislay errors, shaders loader etc)
+ * Code inspired by David Odin's code: Opengl professor at CPE Lyon
  *
  *
 \*****************************************************************************/
+
 
 
 #ifndef GL_UTILS_H
@@ -25,15 +26,21 @@
 
 
 
-
+//Display OpenGL errors
 //Affichage des erreurs OpenGL
 bool print_opengl_error(const char *file, int line);
 
+//Return OpenGL version
 //Retourne la version d'OpenGL
 void get_gl_version(int *major, int *minor);
 
+//Look for a uniform variable in the shader
 //Recherche une variable uniform dans le shader
 GLint get_uni_loc(GLuint program, const GLchar *name);
+
+//Loading shader from a file
+// 1st argument: vertex shader file
+// 2nd argument: Fragment shader file
 
 //Chargement d'un shader un partir d'un fichier
 // 1er argument: fichier contenant le vertex shader
@@ -41,6 +48,8 @@ GLint get_uni_loc(GLuint program, const GLchar *name);
 // Si on ne souhaite charge qu'un shader, on laissera la chaine vide "" pour l'autre nom.
 GLuint read_shader(const std::string &vertex_filename,
                    const std::string &fragment_filename);
+
+
 
 //Decallage d'offset sur pour les buffers des VBO (un simple cast vers un pointeur)
 // Le parametre doit contenir l'offset en octets
